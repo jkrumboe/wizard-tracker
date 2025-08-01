c94f79773c8b9b57d415f1332db8fd4f746530a8
justin <justinkrumbhmer@yahoo.com>
2025-08-01 19:34:15 +0200

    feat: appwrite connection

--- frontend/src/shared/utils/appwrite.js
+++ frontend/src/shared/utils/appwrite.js
@@ -1,7 +1,7 @@
 import { Client, Account, Databases } from "appwrite";
 
 const client = new Client()
-    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setEndpoint(import.meta.env.VITE_APPWRITE_PUBLIC_ENDPOINT)
     .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
